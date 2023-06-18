15e373b6ccbcc384481d833783940e619ec12a99
Matt Yurish <matt18224@gmail.com>
2023-06-18 10:44:34 -0400

    Update

--- src/chip.cc
+++ src/chip.cc
@@ -66,8 +66,9 @@ Napi::Value Chip::getNumberOfLines(const Napi::CallbackInfo& info) {
   int ret = gpiod_chip_num_lines(obj->getNativeChip());
   if(-1 == ret) {
     Napi::Error::New(env,  "::getNumberOfLines() failed").ThrowAsJavaScriptException();
+    return env.Null();
 
-  } else return ret;
+  } else return Napi::Number::New(env, ret);
 }
 
 Napi::Value Chip::getChipName(const Napi::CallbackInfo& info) {
@@ -82,6 +83,7 @@ Napi::Value Chip::getChipName(const Napi::CallbackInfo& info) {
   const char *name = gpiod_chip_name(obj->getNativeChip());
   if(!name) {
     Napi::Error::New(env,  "::getChipName() failed").ThrowAsJavaScriptException();
+    return env.Null();
 
   } else return Napi::String::New(env, name);
 }
@@ -89,16 +91,15 @@ Napi::Value Chip::getChipName(const Napi::CallbackInfo& info) {
 Napi::Value Chip::getChipLabel(const Napi::CallbackInfo& info) {
   Napi::Env env = info.Env();
   DOUT( "%s %s():%d\n", __FILE__, __FUNCTION__, __LINE__);
-  Chip *obj = info.This().Unwrap<Chip>();
+  Chip *obj = Chip::Unwrap(info.This());
   if ( !obj->chip) {
     Napi::Error::New(env,  "::getChipLabel() for chip==NULL").ThrowAsJavaScriptException();
-
     return env.Null();
   }
   const char *label = gpiod_chip_label(obj->getNativeChip());
   if(!label) {
     Napi::Error::New(env,  "::getChipLabel() failed").ThrowAsJavaScriptException();
-
+    return env.Null();
   } else return Napi::String::New(env, label);
 }
 
